98d68a31efcf9c01aa6884ce0bfdb6fbfc45ee30
LanceStasinski <lance.stasinski@gmail.com>
2021-05-17 10:13:41 -0400

    Setup server js file

---
+++ src/server/index.js
@@ -0,0 +1,20 @@
+//load dependencies
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+//start instance and initialize depenedencies
+const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//initialize the dist folder created by webpack
+app.use(express.static('dist'))
+
+//spin up server
+const port = 3030;
+app.listen(port, function () {
+  console.log(`running on localhost: ${port}`)
+})
