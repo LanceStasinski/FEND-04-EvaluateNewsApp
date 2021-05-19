f9a0f2776ad2f90c98b05c84373f8861e0fe6fbd
LanceStasinski <lance.stasinski@gmail.com>
2021-05-19 11:09:35 -0400

    Add analyzeText function

--- src/client/index.js
+++ src/client/index.js
@@ -3,15 +3,9 @@ import { postData } from './js/postData'
 import { getData } from './js/getData'
 import { constructEntry } from './js/constructEntry'
 
-
+//generate NLP results when button is clicked
 const button = document.getElementById('submit');
-button.addEventListener('click', analyzeText);
-
-function analyzeText () {
-  const input = document.getElementById('web-url').value;
-  postData('/add', input)
-  .then(constructEntry(getData('/all')))
-}
+button.addEventListener('click', Client.analyzeText());
 
 //import sass files
 import './styles/_base.scss'
---
+++ src/client/js/analyzeText.js
@@ -0,0 +1,5 @@
+function analyzeText () {
+  const input = document.getElementById('web-url').value;
+  Client.postData('/add', input)
+  .then(Client.getData('/all'))
+}
