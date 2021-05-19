208914d12bef3a108bb509ac3cc8f454c5cee11d
LanceStasinski <lance.stasinski@gmail.com>
2021-05-19 11:51:16 -0400

    Remove clean-webpack-plugin
    Current versions of webpack no longer need the clean-webpack-plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,13 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin  = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/client/index.js',
   output: {
     libraryTarget: 'var',
-    library: 'Client'
+    library: 'Client',
+    clean: true,
   },
   mode: 'development',
   devtool: 'source-map',
@@ -28,12 +28,6 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './src/client/views/index.html',
       filename: './index.html'
-    }),
-    new CleanWebpackPlugin({
-      dry: true,
-      verbose: true,
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false,
     })
   ]
 }
