44dd566e57e315bd775dfc6d4975f46038348e5e
LanceStasinski <lance.stasinski@gmail.com>
2021-05-21 14:47:06 -0400

    Fix post requets, remove extra get route

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,3 @@
-//set endpoint
-projectData = [];
-
 //get api key
 const dotenv = require('dotenv')
 dotenv.config()
@@ -11,7 +8,6 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const { json } = require('body-parser')
 
 //start instance and initialize depenedencies
 const app = express()
@@ -32,50 +28,15 @@ app.post('/add', getNLPData);
 
 //POST request to meaningcloud.com
 function getNLPData (req, res) {
-  const newsURL = req.body.url;
+  const newsURL = req.body.data;
   const results = await fetch(`https://api.meaningcloud.com/sentiment-2.1=${apiKey}&url=${newsURL}`,
   { method: 'POST'});
 
-
-
-
-
-  console.log(req.body);
-  //create new data object that will be sent to meaningcloud
-  const formData = new FormData();
-  //add personal api key to object
-  formData.append("key", apiKey);
-  //add user-entered url to object
-  formData.append("url", req.body);
-  //set language to auto, response to english, and include detailed response
-  formData.append("lang", "auto");
-  formData.append("ilang", "en");
-
-  //post request that should result in a new object containing the response from
-  //meaningcloud.
-  const apiInfo = postAPI('https://api.meaningcloud.com/sentiment-2.1', formData)
-  //only add new object to projectData once the request has been completed
-  .then(projectData.push(apiInfo))
-}
-
-const postAPI = async (url = '', data = {}) => {
-  const response = await fetch(url, {
-    method: 'POST',
-    body: data,
-    redirect: 'follow'
-  });
-
   try {
-    const newData = await response.json();
-    return newData;
-  } catch(error) {
-    console.log('error', error);
+    const apiData = await results.json();
+    res.send(apiData);
+  } catch (error) {
+    console.log("error", error)
   }
 }
 
-//GET route
-app.get('/all', sendData)
-
-function sendData (req, res) {
-  res.send(projectData[0])
-}
