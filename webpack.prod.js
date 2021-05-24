63da8d19de0ff2f7891fe3b35544772c85da069a
LanceStasinski <lance.stasinski@gmail.com>
2021-05-24 12:06:46 -0400

    Add service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,19 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-//const WorkboxPlugin = require('workbox-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
   output: {
-    libraryTarget: 'var',
-    library: 'Client'
-  },
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'myLibrary',
+    libraryTarget: 'umd',
+   },
   mode: 'production',
   module: {
     rules: [
@@ -22,14 +26,20 @@ module.exports = {
         test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
-    ]
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin({}),
+      new TerserPlugin({})
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/client/views/index.html',
       filename: './index.html'
     }),
-    //new WorkboxPlugin.GenerateSW(),
+    new WorkboxPlugin.GenerateSW(),
     new MiniCssExtractPlugin({filename: '[name].css'})
   ]
 }
