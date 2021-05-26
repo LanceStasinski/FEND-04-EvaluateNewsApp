201ee78b157612b2376c22bef98508e09ba316dd
LanceStasinski <lance.stasinski@gmail.com>
2021-05-26 13:22:05 -0400

    Add service workers to the webpack dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin  = require('html-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = {
   entry: path.resolve(__dirname, './src/client/index.js'),
@@ -37,6 +39,7 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './src/client/views/index.html',
       filename: './index.html'
-    })
+    }),
+    new WorkboxPlugin.GenerateSW()
   ]
 }
